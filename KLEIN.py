5e79ede478fce05430d5be569e42a0361716ead6
Joost Rijneveld <joostrijneveld@gmail.com>
2014-07-15 17:39:16 +0200

    Wrapped KLEIN in a class

--- KLEIN.py
+++ KLEIN.py
@@ -3,29 +3,35 @@
 SBOX = [0x7, 0x4, 0xA, 0x9, 0x1, 0xF, 0xB, 0x0,
         0xC, 0x3, 0x2, 0x6, 0x8, 0xE, 0xD, 0x5]
 
-def addRoundKey(state, sk):
-    pass
+class KLEIN(object):
 
-def subNibbles(state):
-    pass
+    def __init__(self, nr=12, size=64):
+        self.nr = nr
+        self.size = size
 
-def rotateNibbles(state):
-    pass
+    def addRoundKey(self, state, sk):
+        pass
 
-def mixNibbles(state):
-    pass
-    
-def keySchedule(sk, i):
-    pass
+    def subNibbles(self, state):
+        pass
 
-def encrypt(plaintext, key, nr=12, size=64):
-    state = plaintext
-    sk = key
-    for i in range(nr):
-        state = addRoundKey(state, sk)
-        state = subNibbles(state)
-        state = rotateNibbles(state)
-        state = mixNibbles(state)
-        sk = keySchedule(sk, i)
-    state = addRoundKey(state, sk)
-    return state
+    def rotateNibbles(self, state):
+        pass
+
+    def mixNibbles(self, state):
+        pass
+
+    def keySchedule(self, sk, i):
+        pass
+
+    def encrypt(self, plaintext, key, nr=12, size=64):
+        state = plaintext
+        sk = key
+        for i in range(nr):
+            state = self.addRoundKey(state, sk)
+            state = self.subNibbles(state)
+            state = self.rotateNibbles(state)
+            state = self.mixNibbles(state)
+            sk = self.keySchedule(sk, i)
+        state = self.addRoundKey(state, sk)
+        return state
